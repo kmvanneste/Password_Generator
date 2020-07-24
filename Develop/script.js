b71386f7a543356bca57ce39c012eab95a730200
kmvanneste <kmvanneste17@gmail.com>
2020-07-23 21:27:42 -0400

    Working JS code

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers = "1234567890".split("");
+var symbol = "!#$%&*+<=>?@/[]^_`|~".split("");
+var myArray = [];
+var finalPassword = [];
+
+// User Prompts
+var charCount = prompt("How many characters would you like your password to be?");
+var upperLetters = confirm("Press OK if you want uppercase letters in your password.");
+var lowerLetters = confirm("Press OK if you want lowercase letters in your password.");
+var userNumbers = confirm("Press OK if you want numbers in your password.");
+var userSpecialChar = confirm("Press OK if you want special characters in your password.");
+
+function generatePassword(howManyChar, upLet, lowLet, numb, char) {
+
+  if (howManyChar >= 8 && howManyChar <= 128) {
+      if (upLet === true) {
+        for (var i = 0; i < upperLetters.length; i++) {
+          myArray.push(upperCase[i]);
+          console.log("User set Upper case letters to True");
+        }
+      } 
+      if (lowLet === true) {
+        for (var i = 0; i < lowerLetters.length; i++) {
+        myArray.push(lowerCase[i]);
+        console.log("User set Upper case letters to True");
+          }
+        } 
+      if (numb === true) {  
+        for (var i = 0; i < userNumbers.length; i++) {
+        myArray.push(numbers[i]);
+        console.log("User set Upper case letters to True");
+          }
+        } 
+      if (char === true) {  
+        for (var i = 0; i < userSpecialChar.length; i++) {
+        myArray.push(symbol[i]);
+        console.log("User set Upper case letters to True");
+          }
+        } 
+
+      for (var i = 0; i < howManyChar; i++) {
+        var randomNum = Math.floor(Math.random * (myArray.length));
+        console.log(randomNum);
+
+        result.push(myArray[randomNum]);
+      }
+    }  
+  else {
+  alert("Please create a password between 8 and 128 characters long.");
+  return location.reload();
+  }      
+  return finalPassword.join("");
+}
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(charCount, upperLetters, lowerLetters, userNumbers, userSpecialChar);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
