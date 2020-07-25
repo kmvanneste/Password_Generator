5f91928a0a2a4a33be73d3ba42f915ae4792d98f
kmvanneste <kmvanneste17@gmail.com>
2020-07-25 18:12:10 -0400

    Fixed question order, prompt variables moved from global scope to local scope

--- Develop/script.js
+++ Develop/script.js
@@ -6,41 +6,43 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var numbers = "1234567890".split("");
 var symbol = "!#$%&*+<=>?@/[]^_`|~".split("");
 
-// User Prompts
-var charCount = prompt("How many characters would you like your password to be?");
-var upperLetters = confirm("Press OK if you want uppercase letters in your password.");
-var lowerLetters = confirm("Press OK if you want lowercase letters in your password.");
-var userNumbers = confirm("Press OK if you want numbers in your password.");
-var userSpecialChar = confirm("Press OK if you want special characters in your password.");
 
-function generatePassword(howManyChar, upLet, lowLet, numb, char) {
+function generatePassword() {
   var myArray = [];
   var finalPassword = [];
-  //Converting howManyChar from a string to an integer
+  
+  var howManyChar = prompt("How many characters would you like your password to be?");
   var howManyChar = parseInt(howManyChar);
-
   if (howManyChar < 8 || howManyChar > 128) {
     alert("Please create a password between 8 and 128 characters long.");
     return location.reload();
   }
+
+  var upLet = confirm("Press OK if you want uppercase letters in your password.");
   if (upLet === true) {
     for (var i = 0; i < upperCase.length; i++) {
       myArray.push(upperCase[i]);
       console.log("User set Upper case letters to True");
     }
   }
+
+  var lowLet = confirm("Press OK if you want lowercase letters in your password.");
   if (lowLet === true) {
     for (var i = 0; i < lowerCase.length; i++) {
       myArray.push(lowerCase[i]);
       console.log("User set Lower case letters to True");
     }
   }
+
+  var numb = confirm("Press OK if you want numbers in your password.");
   if (numb === true) {
     for (var i = 0; i < numbers.length; i++) {
       myArray.push(numbers[i]);
       console.log("User set numbers to True");
     }
   }
+
+  var char = confirm("Press OK if you want special characters in your password.");
   if (char === true) {
     for (var i = 0; i < symbol.length; i++) {
       myArray.push(symbol[i]);
@@ -62,7 +64,7 @@ function generatePassword(howManyChar, upLet, lowLet, numb, char) {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(charCount, upperLetters, lowerLetters, userNumbers, userSpecialChar);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
